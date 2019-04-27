e9859c68349fb01e2f8da0dc435f57257b6e1ada
psarnai1 <Parikshit Sarnaik>
2019-04-27 19:43:04 -0400

    Added code to get reading from magnetometer and calculate metal power around the device

--- app/src/main/java/com/example/metaldetector/MainActivity.java
+++ app/src/main/java/com/example/metaldetector/MainActivity.java
@@ -1,13 +1,43 @@
 package com.example.metaldetector;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor magnetSensor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        magnetSensor = sensorManager.getSensorList(Sensor.TYPE_MAGNETIC_FIELD).get(0);
+        sensorManager.registerListener(this, magnetSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor == magnetSensor) {
+            float x = event.values[0];
+            float y = event.values[1];
+            float z = event.values[2];
+            double metalpower = Math.round(Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2)));
+
+            Toast.makeText(getApplicationContext(),"MetalPower: " + metalpower, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
